354b57515a6f6179389d8e678677beee0f464f00
IgorR <igorrozhnov95@gmail.com>
2022-11-09 22:08:35 +0300

    add foreign keys, relationship

--- models.py
+++ models.py
@@ -1,40 +1,49 @@
 from flask import Flask
-
-from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
+from flask_sqlalchemy import SQLAlchemy
 
-app=Flask(__name__)
-db=SQLAlchemy(app)
-migrate=Migrate(app, db)
+app = Flask(__name__)
+app.debug = True
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
+
 
 class Dealer (db.Model):
-    __tablename__ = 'skodadealers'
-    id = db.Column(db.Integer, primary_key=True)
+    __tablename__ = 'skoda_dealers'
+    id = db.Column(db.Integer(), primary_key=True)
     name = db.Column(db.String(100))
     city = db.Column(db.String(100))
-    
+
     def __init__(self, name, city):
         self.name = name
         self.city = city
-        
+
     def dealerdict(self):
         return {'name': self.name, 'city': self.city}
-    
-    
+
+
+skodadealers_cars = db.Table("skodadealers_cars",
+    db.Column('dealers_id', db.Integer(), db.ForeignKey('skoda_dealers.id')),
+    db.Column('cars_id', db.Integer(), db.ForeignKey('skoda_cars.id'))
+    )
+
+
 class Car (db.Model):
-    __tablename__ = 'skodacars'
-    id = db.Column(db.Integer, primary_key=True)    
+    __tablename__ = 'skoda_cars'
+    id = db.Column(db.Integer(), primary_key=True)
     equipment = db.Column(db.String(100))
     model = db.Column(db.String(100))
     price = db.Column(db.Integer())
-    
+    dealers = db.relationship('Dealer', secondary = skodadealers_cars, backref = 'cars')
+
     def __init__(self, equipment, model, price):
         self.equipment = equipment
         self.model = model
         self.price = price
-        
+
     def cardict(self):
-        return {'equipment': self.equipment, 'model': self.model, 'price': self.price}    
-    
+        return {'equipment': self.equipment, 'model': self.model, 'price': self.price}
+
+
 if __name__ == '__main__':
     app.run()
