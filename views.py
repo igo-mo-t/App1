5962ba286d71f1ac722f463f1b0dede66a426d0c
IgorR <igorrozhnov95@gmail.com>
2022-11-11 21:18:18 +0300

    Release v1

--- views.py
+++ views.py
@@ -16,6 +16,7 @@ def create_table():
     
 
 class DealersView(Resource):
+    
     def post(self):
         data = request.get_json()
         new_dealer = Dealer(data['name'], data['city'])
@@ -29,6 +30,7 @@ class DealersView(Resource):
     
     
 class CarsView(Resource):
+    
     def post(self):
         data = request.get_json()
         all_dealers=Dealer.query.all()
@@ -46,7 +48,9 @@ class CarsView(Resource):
         all_cars = Car.query.all()
         return {'All cars':list(x.cardict() for x in all_cars)}
 
+
 class DealerView(Resource):
+    
     def get(self,name):
         dealer = Dealer.query.filter_by(name=name).first()
         if dealer:
@@ -61,12 +65,14 @@ class DealerView(Resource):
         if dealer:
             dealer.name = data['name']
             dealer.city = data['city']
+            db.session.commit()
+            return {'Update Dealer':dealer.dealerdict()}
         else:
             dealer=Dealer(name=name, city=data['city']) 
         
         db.session.add(dealer)
         db.session.commit()
-        return dealer.dealerdict() 
+        return {'New dealer':dealer.dealerdict()} 
     
     def delete(self,name):
         dealer = Dealer.query.filter_by(name=name).first()
@@ -74,12 +80,54 @@ class DealerView(Resource):
             db.session.delete(dealer)
             db.session.commit()
             return {'message': 'Deleted'}
+        
+        return {'message': 'Dealer not found'}, 404
+        
+        
+class CarView(Resource):
+    
+    def get(self,model):
+        car = Car.query.filter_by(model=model).first()
+        if car:
+            dealers = car.dealers
+            return {f'Car {model}':car.cardict(), 'Dealers':list(x.dealerdict() for x in dealers)}
+    
+        return {'Message': 'Car not found'}, 404
+     
+    def put(self, model):
+        data = request.get_json()
+        car = Car.query.filter_by(model=model).first()
+        if car:
+             car.equipment = data['equipment']
+             car.model = data['model']
+             car.price = data['price']
         else:
-            return {'message': 'Dealer not found'}, 404
+             car = Car(equipment = data['equipment'], model=model, price = data['price'])
+             dealers = Dealer.query.filter_by(city=data['city']).all()
+             for x in dealers:
+                 x.cars.append(car)
+                 db.session.add(x)
+                 db.session.commit()
+                 return {'New Car':car.cardict(), 'Dealers':list(y.dealerdict() for y in dealers)}, 201    
+        
+        db.session.commit()
+        return {'Update Car':car.cardict()}
+        
+    def delete(self, model):
+        car = Car.query.filter_by(model=model).first()
+        if car:
+            db.session.delete(car)
+            db.session.commit()
+            return {'Message': 'Deleted'}
+        return {'Message': 'Car not found'}, 404   
+                   
+         
+             
         
 api.add_resource(DealersView, '/dealers')        
 api.add_resource(CarsView, '/cars')
 api.add_resource(DealerView, '/dealer/<string:name>')
+api.add_resource(CarView, '/car/<string:model>')
         
 if __name__ == '__main__':
     app.run()         
